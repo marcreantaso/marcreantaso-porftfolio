3af019b479dda1951c6d9ea2155c004b69d10ae2
Marc Reantaso <120691076+marcreantaso@users.noreply.github.com>
2025-10-18 12:40:19 +0800

    Delete ekomerzz directory

--- ekomerzz/jscom.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", function() {
-    const introContainer = document.querySelector(".intro-container");
-
-    setTimeout(function() {
-        introContainer.classList.add("show");
-    }, 1000); // Add a 1-second delay before the fade-in effect
-});
